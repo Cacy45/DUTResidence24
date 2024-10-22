872c331105c1fbfb78a019a03d2fe8319f24bd93
Cacy45 <lcacilda2@gmail.com>
2024-10-23 00:20:13 +0200

    refactor: Update entrypoint.sh to start Django with nohup

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,8 @@
 # Activate the virtual environment
 . /app/venv/bin/activate
 
-# Start Django
-python3 /app/Residence24/manage.py runserver 0.0.0.0:8000 &
+# Start Django with nohup
+nohup python3 /app/Residence24/manage.py runserver 0.0.0.0:8000 &
 
 # Start Nginx
 nginx -g "daemon off;"
