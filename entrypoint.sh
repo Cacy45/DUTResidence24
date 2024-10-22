02ddbacfaa2bfcf57c5c0f9a90099ab1f18c4036
Cacy45 <lcacilda2@gmail.com>
2024-10-22 23:52:18 +0200

    refactor: Update entrypoint.sh to use absolute path for Django runserver command

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Start Django
-python3 ./Residence24/manage.py runserver 0.0.0.0:8000 &
+python3 /app/Residence24/manage.py runserver 0.0.0.0:8000 &
 
 # Start Nginx
 nginx -g "daemon off;"
